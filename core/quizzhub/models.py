9a86f7a70ea3f77ac1516b6088de49998bdcaa88
nightmare3006 <nightmare000630@gmail.com>
2024-10-06 20:06:36 -0400

    Core: added models and first migration

--- core/quizzhub/models.py
+++ core/quizzhub/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Title')
+    description = models.TextField(max_length=1000, verbose_name= 'Description')
+    post_date = models.DateTimeField(default=timezone.now)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Owner')
+
+    def __str__(self) -> str:
+        return self.title
+
+class Solution(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete= models.CASCADE, verbose_name='Quiz')
+    owner = models.ForeignKey(User, on_delete= models.CASCADE, verbose_name='Owner')
+    content = models.TextField(max_length=500,verbose_name='Content')
+    posted_at = models.DateTimeField(default=timezone.now)
+
+
+
+class Winner(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete= models.CASCADE, verbose_name='Quiz')
+    solution = models.ForeignKey(Solution, on_delete=models.CASCADE, verbose_name='Solution')
