d8ad093db6b8bcc7e7e929b936cae7bd43f3d756
nightmare3006 <nightmare000630@gmail.com>
2024-10-08 00:31:14 -0400

    core: added some simple views for testing

--- core/quizzhub/views.py
+++ core/quizzhub/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .serializer import *
+from rest_framework import viewsets
+from .models import *
 
 # Create your views here.
+class QuizViewSet(viewsets.ModelViewSet):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+
+
+class SolutionViewSet(viewsets.ModelViewSet):
+    queryset = Solution.objects.all()
+    serializer_class = SolutionSerializer
+
+class WinnerViewSet(viewsets.ModelViewSet):
+    queryset = Winner.objects.all()
+    serializer_class = WinnerSerializer
