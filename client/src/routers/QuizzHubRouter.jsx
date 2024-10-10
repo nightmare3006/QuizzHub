575e9d3c89c5d0bf70ff805c782eaccc9ff3838d
nightmare3006 <nightmare000630@gmail.com>
2024-10-09 22:05:53 -0400

    client: added some private routes

--- client/src/routers/QuizzHubRouter.jsx
+++ client/src/routers/QuizzHubRouter.jsx
@@ -1,15 +1,23 @@
 import { Route, Routes } from "react-router-dom"
-import { QuizListPage } from "../components/QuizListPage"
-import { SolutionListPage } from "../components/SolutionListPage"
-import { QuizDetailPage } from "../components/QuizDetailPage"
+import { QuizListPage } from "../components/Quiz/QuizListPage"
+import { SolutionListPage } from "../components/Solution/SolutionListPage"
+import { QuizDetailPage } from "../components/Quiz/QuizDetailPage"
+import { CreateQuiz } from "../components/Quiz/CreateQuiz"
+import { PrivateRoute } from "../components/PrivateRoute"
 
 
 export const QuizzHubRouter = () => {
     return (
         <Routes>
             <Route path="list/" element={<QuizListPage />} />
-            <Route path="solutions/" element={<SolutionListPage />} />
-            <Route path="quiz/detail" element={<QuizDetailPage />} />
+            <Route path="solutions/" element={<PrivateRoute>
+                <SolutionListPage />
+            </PrivateRoute>} />
+            <Route path="quiz/detail/" element={<QuizDetailPage />} />
+            {/* <Route path="quiz/:quizId/detail" element={<QuizDetailPage />} /> */}
+            <Route path="quiz/create" element={<PrivateRoute>
+                <CreateQuiz />
+            </PrivateRoute>} />
         </Routes>
     )
 }
