cb5d2336b397b114748178cb58445de74727f2cb
nightmare3006 <nightmare000630@gmail.com>
2024-10-10 14:05:18 -0400

    core: Add validation for unique solution per user and soft delete for solutions

--- core/quizzhub/views.py
+++ core/quizzhub/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .serializer import *
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import *
 
 # Create your views here.
@@ -17,6 +18,20 @@ class QuizViewSet(viewsets.ModelViewSet):
 class SolutionViewSet(viewsets.ModelViewSet):
     queryset = Solution.objects.all()
     serializer_class = SolutionSerializer
+
+    def perform_create(self, serializer):
+        quiz_id = self.request.data.get('quiz')
+        quiz = Quiz.objects.get(id=quiz_id)
+        existing_solution = Solution.objects.filter(quiz=quiz, owner=self.request.user)
+        if existing_solution:
+            raise serializers.ValidationError('You have already posted a solution for this quiz')
+        serializer.save(owner=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.deleted = True
+        instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class WinnerViewSet(viewsets.ModelViewSet):
     queryset = Winner.objects.all()
