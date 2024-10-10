2f6ea1e5d2039a94689bbf36724104c28a3fec6c
nightmare3006 <nightmare000630@gmail.com>
2024-10-09 22:01:50 -0400

    client: added SolutionBody component

---
+++ client/src/components/Solution/SolutionBody.jsx
@@ -0,0 +1,15 @@
+
+
+export const SolutionBody = () => {
+  return (
+    <div className="bd-example m-0 border-0">
+        <div className="alert alert-success" role="alert">
+          <h4 className="alert-heading">Well done!</h4>
+          <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of tex</p>
+          <hr />
+          <p className="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+        </div>
+        
+  </div>
+  )
+}
