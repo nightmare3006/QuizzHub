e48b31b5ef837589f2a9ed08d9338abfd3a63771
nightmare3006 <nightmare000630@gmail.com>
2024-10-09 21:40:01 -0400

    client: added Pagination component

---
+++ client/src/components/Pagination.jsx
@@ -0,0 +1,22 @@
+
+
+export const Pagination = ({ next, previous, onPageChange }) => {
+
+    console.log(next, previous)
+    return (
+      <nav>
+        <ul className="pagination pagination-lg justify-content-center">
+          {previous && (
+            <li className="page-item">
+              <button className="btn btn-outline-primary" onClick={() => onPageChange(previous)}><i className="fas fa-lg fa-angle-double-left"></i></button>
+            </li>
+          )}
+          {next && (
+            <li className="page-item">
+              <button className="btn btn-outline-primary" onClick={() => onPageChange(next)}><i className="fas fa-lg fa-angle-double-right"></i></button>
+            </li>
+          )}
+        </ul>
+      </nav>
+    );
+  };
