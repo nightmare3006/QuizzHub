715309cb61cce8d74bda36a9f15417c9fcd5a451
nightmare3006 <nightmare000630@gmail.com>
2024-10-09 22:02:10 -0400

    client: added SolutionListPage component

---
+++ client/src/components/Solution/SolutionListPage.jsx
@@ -0,0 +1,10 @@
+import { SolutionBody } from "./SolutionBody"
+
+
+export const SolutionListPage = () => {
+  return (
+    <div className="p-4 container shadow rounded-3 ">
+      <SolutionBody />
+    </div>
+  )
+}
