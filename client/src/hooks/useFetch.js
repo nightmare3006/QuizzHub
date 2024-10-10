a832611d7fd2ce56b3fc91e8d48f9e7ff8e3662f
nightmare3006 <nightmare000630@gmail.com>
2024-10-10 14:24:57 -0400

    client: fix useFetch for private and public routes, and improved token refresh

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,33 +1,45 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { refreshToken, updateToken } from "../helpers/authService";
 
 export const useFetch = (url) => {
     const [state, setState] = useState({ data: [], loading: true, errors: null, next: null, previous: null });
-
     useEffect(() => {
         setState({ data: [], loading: true, errors: null, next: null, previous: null });
+        const user = JSON.parse(localStorage.getItem('user'));
+        const token = user ? updateToken() : null;
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        };
 
-        axios.get(url)
-            .then(response => {
-                setState({
-                    loading: false,
-                    errors: null,
-                    data: response.data.results,
-                    next: response.data.next,
-                    previous: response.data.previous,
-                });
-            })
-            .catch(error => {
-                setState({
-                    loading: false,
-                    errors: error.message,
-                    data: [],
-                    next: null, 
-                    previous: null,
+        if (token) {
+            config.headers['Authorization'] = `JWT ${token}`;
+        }
 
-                });
+        axios.get(url, config )
+        .then(response => {
+            setState({
+                loading: false,
+                errors: null,
+                data: response.data.results ?? response.data,
+                next: response.data.next ?? null,
+                previous: response.data.previous ?? null,
             });
-    }, [url]);
+        })
+        .catch(error => {
+            setState({
+                loading: false,
+                errors: error.message,
+                data: [],
+                next: null,
+                previous: null,
 
-    return state;
+            });
+        });
+}, [url]);
+
+return state;
 };
