a646caa5f6fd1924f66329050f0dcbe394b0986a
nightmare3006 <nightmare000630@gmail.com>
2024-10-10 14:38:57 -0400

    client: Add handlePost, setContent, and content to TextForm component

--- client/src/components/TextForm.jsx
+++ client/src/components/TextForm.jsx
@@ -1,14 +1,14 @@
 
 
-export const TextForm = () => {
+export const TextForm = ({content, setContent, handlePost}) => {
     return (
         <>
             <h5 className="mt-3">Share your Ideas!</h5>
-            <form>
+            <form onSubmit={handlePost}>
                 <div className="form-group">
                     <div className="row mt-3 mb-2">
                         <div className="col-12 col-md-11">
-                            <textarea className="form-control" style={{ border: "1px solid white", resize: "none", height: "100px" }} placeholder="write anithing you whant to share" />
+                            <textarea value={content} className="form-control" style={{ border: "1px solid white", resize: "none", height: "100px" }} onChange={e => setContent(e.target.value) }  placeholder="write anithing you whant to share" required/> 
                         </div>
                         <div className="col-12 col-md-1 d-flex justify-content-center align-items-end mt-2 mt-md-0">
 
