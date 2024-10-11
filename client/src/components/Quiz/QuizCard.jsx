99d03a4b53e929606926f26f8d3e83cb37339eac
nightmare3006 <nightmare000630@gmail.com>
2024-10-10 22:30:42 -0400

    client: fix missingtext data from  the databases

--- client/src/components/Quiz/QuizCard.jsx
+++ client/src/components/Quiz/QuizCard.jsx
@@ -31,14 +31,12 @@ export const QuizCard = ({ id, title, post_date, owner, description, setUpdate }
     }
 
     return (
-        <div className="card text-center shadow" style={{ maxWidth: "82%", minWidth: "350px" }}>
+        <div className="card text-center shadow" style={{ maxWidth: "82%", minWidth: "450px", minHeight: "250px" }}>
             <div className="card-header bg-dark text-light">
                 <h5 className="card-title">{title}</h5>
             </div>
             <div className="card-body p-4">
-                <p className="card-text text-justify">This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.This is some.</p>
-
-                <p className="text-muted">Solutions: 0</p>
+                <p className="card-text text-justify">{description}</p>
             </div>
             <div className="card-footer">
                 <div className="d-flex justify-content-between align-items-center">
